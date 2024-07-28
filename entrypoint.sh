d3cca318c1ee99544cb198a5ae487134776886af
Owen Blount <oblount077@gmail.com>
2024-07-28 15:41:46 +0100

    removed shell from workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/sh -l
 ## A bash script for the job to push select files to pastebin.com
 ## DIR_TO_UPLOAD -- The path to the dir to search files in
 ## EXT_WHITELIST -- A comma seperated string whitelisting file extensions
