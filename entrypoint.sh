2b50ca6b1e3ce1f4a770ebc4cb9094197162ce8f
Owen Blount <oblount077@gmail.com>
2024-07-28 15:47:39 +0100

    sh compatible

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 ## A bash script for the job to push select files to pastebin.com
 ## DIR_TO_UPLOAD -- The path to the dir to search files in
-## EXT_WHITELIST -- A comma seperated string whitelisting file extensions
+## EXT_WHITELIST -- A comma separated string whitelisting file extensions
 
 DIR_TO_UPLOAD=$1
 EXT_WHITELIST=$2
@@ -16,18 +16,18 @@ if [ ! -d "$DIR_TO_UPLOAD" ]; then
   exit 1
 fi
 
-IFS=',' read -r -a EXTENSIONS <<< "$EXT_WHITELIST"
+EXTENSIONS=$(echo $EXT_WHITELIST | tr ',' ' ')
 
 echo "PASTEBIN_API_KEY is set"
 echo "Directory to upload: $DIR_TO_UPLOAD"
-echo "Allowed extensions: ${EXTENSIONS[@]}"
+echo "Allowed extensions: $EXTENSIONS"
 echo '### :pencil: Report' >> $GITHUB_STEP_SUMMARY
 echo '| Pastebin Link :link: | File :page_facing_up: |' >> $GITHUB_STEP_SUMMARY
 echo '| :---                 | :----:                |' >> $GITHUB_STEP_SUMMARY
 echo "----"
 
-for EXT in "${EXTENSIONS[@]}"; do
-  while IFS= read -r FILE; do
+for EXT in $EXTENSIONS; do
+  find "$DIR_TO_UPLOAD" -type f -name "*.$EXT" | while IFS= read -r FILE; do
     echo "Uploading $FILE to Pastebin"
     FILE_CONTENTS=$(cat "$FILE")
     RESPONSE=$(curl -s -X POST \
@@ -38,7 +38,7 @@ for EXT in "${EXTENSIONS[@]}"; do
 
     echo "Response from Pastebin: $RESPONSE"
     echo "| $RESPONSE | $FILE |" >> $GITHUB_STEP_SUMMARY
-  done < <(find "$DIR_TO_UPLOAD" -type f -name "*.$EXT")
+  done
 done
 
 exit 0
